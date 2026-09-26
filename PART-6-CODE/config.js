d42551d5390550b0fa85d55dba85098646e39c5d
clairelabelle1975 <claire.labelle1975@gmail.com>
2026-09-26 03:57:55 +0200

    Add files via upload

---
+++ PART-6-CODE/config.js
@@ -0,0 +1,39 @@
+window.CLAIRE_SITE = {
+  socials: {
+    instagram: "https://www.instagram.com/claire_velvetmuse/",
+    x: "https://x.com/claireVelvetX",
+    fanvue: "https://www.fanvue.com/clairevogue",
+    tiktok: "#"
+  },
+  heroVideo: "assets/videos/clip-01.mp4",
+  heroPoster: "assets/images/claire-01.jpg",
+  gallery: [
+    "assets/images/claire-01.jpg",
+    "assets/images/claire-02.jpg",
+    "assets/images/claire-03.jpg",
+    "assets/images/claire-04.jpg",
+    "assets/images/claire-05.jpg"
+  ],
+  videos: [
+    { src: "assets/videos/showcase/claire-01.mp4", poster: "assets/posters/showcase/claire-01.jpg", label: "CLAIRE / 01" },
+    { src: "assets/videos/showcase/claire-02.mp4", poster: "assets/posters/showcase/claire-02.jpg", label: "CLAIRE / 02" },
+    { src: "assets/videos/showcase/claire-03.mp4", poster: "assets/posters/showcase/claire-03.jpg", label: "CLAIRE / 03" },
+    { src: "assets/videos/showcase/claire-04.mp4", poster: "assets/posters/showcase/claire-04.jpg", label: "CLAIRE / 04" },
+    { src: "assets/videos/showcase/claire-05.mp4", poster: "assets/posters/showcase/claire-05.jpg", label: "CLAIRE / 05" },
+    { src: "assets/videos/showcase/claire-06.mp4", poster: "assets/posters/showcase/claire-06.jpg", label: "CLAIRE / 06" },
+    { src: "assets/videos/showcase/claire-07.mp4", poster: "assets/posters/showcase/claire-07.jpg", label: "CLAIRE / 07" },
+    { src: "assets/videos/showcase/claire-08.mp4", poster: "assets/posters/showcase/claire-08.jpg", label: "CLAIRE / 08" },
+    { src: "assets/videos/showcase/claire-09.mp4", poster: "assets/posters/showcase/claire-09.jpg", label: "CLAIRE / 09" },
+    { src: "assets/videos/showcase/claire-10.mp4", poster: "assets/posters/showcase/claire-10.jpg", label: "CLAIRE / 10" },
+    { src: "assets/videos/showcase/claire-11.mp4", poster: "assets/posters/showcase/claire-11.jpg", label: "CLAIRE / 11" },
+    { src: "assets/videos/showcase/claire-12.mp4", poster: "assets/posters/showcase/claire-12.jpg", label: "CLAIRE / 12" },
+    { src: "assets/videos/showcase/claire-13.mp4", poster: "assets/posters/showcase/claire-13.jpg", label: "CLAIRE / 13" },
+    { src: "assets/videos/showcase/claire-14.mp4", poster: "assets/posters/showcase/claire-14.jpg", label: "CLAIRE / 14" },
+    { src: "assets/videos/showcase/claire-15.mp4", poster: "assets/posters/showcase/claire-15.jpg", label: "CLAIRE / 15" },
+    { src: "assets/videos/showcase/claire-16.mp4", poster: "assets/posters/showcase/claire-16.jpg", label: "CLAIRE / 16" },
+    { src: "assets/videos/showcase/claire-17.mp4", poster: "assets/posters/showcase/claire-17.jpg", label: "CLAIRE / 17" },
+    { src: "assets/videos/showcase/claire-18.mp4", poster: "assets/posters/showcase/claire-18.jpg", label: "CLAIRE / 18" },
+    { src: "assets/videos/showcase/claire-19.mp4", poster: "assets/posters/showcase/claire-19.jpg", label: "CLAIRE / 19" },
+    { src: "assets/videos/showcase/claire-20.mp4", poster: "assets/posters/showcase/claire-20.jpg", label: "CLAIRE / 20" }
+  ]
+};
